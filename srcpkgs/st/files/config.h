98161badc8dceecb56f66334719204397d1f1bd9
copyleftie <copyleftie@protonmail.com>
2018-02-10 16:24:26 -0500

    st: use base16-woodland

--- srcpkgs/st/files/config.h
+++ srcpkgs/st/files/config.h
@@ -82,42 +82,44 @@ char termname[] = "st-256color";
  */
 static unsigned int tabspaces = 8;
 
-/* Terminal colors (16 first used in escape sequence) */
+/*
+* base16-woodland-theme.h 
+*
+* Base16: (https://github.com/chriskempson/base16)
+*
+* Authors:
+*
+* Scheme: Jay Cornwall (https://jcornwall.com)
+* Template: Honza Pokorny <me@honza.ca>
+*
+*/
 const char *colorname[] = {
-
-  /* 8 normal colors */
-  [0] = "#000000", /* black   */
-  [1] = "#ff5555", /* red     */
-  [2] = "#50fa7b", /* green   */
-  [3] = "#f1fa8c", /* yellow  */
-  [4] = "#bd93f9", /* blue    */
-  [5] = "#ff79c6", /* magenta */
-  [6] = "#8be9fd", /* cyan    */
-  [7] = "#bbbbbb", /* white   */
-
-  /* 8 bright colors */
-  [8]  = "#44475a", /* black   */
-  [9]  = "#ff5555", /* red     */
-  [10] = "#50fa7b", /* green   */
-  [11] = "#f1fa8c", /* yellow  */
-  [12] = "#bd93f9", /* blue    */
-  [13] = "#ff79c6", /* magenta */
-  [14] = "#8be9fd", /* cyan    */
-  [15] = "#ffffff", /* white   */
-
-  /* special colors */
-  [256] = "#282a36", /* background */
-  [257] = "#f8f8f2", /* foreground */
+  "#302b25", /* base01 */
+  "#d35c5c", /* base08 */
+  "#b7ba53", /* base0B */
+  "#e0ac16", /* base0A */
+  "#88a4d3", /* base0D */
+  "#b49368", /* base0F */
+  "#6eb958", /* base0C */
+  "#d7c8bc", /* base06 */
+  "#231e18", /* base00 */
+  "#ca7f32", /* base09 */
+  "#48413a", /* base02 */
+  "#9d8b70", /* base03 */
+  "#b4a490", /* base04 */
+  "#bb90e2", /* base0E */
+  "#cabcb1", /* base05 */
+  "#e4d4c8", /* base07 */
 };
 
 /*
- * Default colors (colorname index)
- * foreground, background, cursor
- */
-unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
-unsigned int defaultcs = 257;
-unsigned int defaultrcs = 257;
+* Default colors (colorname index)
+* foreground, background, cursor, reverse cursor
+*/
+unsigned int defaultfg = 12;
+unsigned int defaultbg = 8;
+unsigned int defaultcs = 14;
+unsigned int defaultrcs = 15;
 
 /*
  * Colors used, when the specific fg == defaultfg. So in reverse mode this
