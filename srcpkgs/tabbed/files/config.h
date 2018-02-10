ecbf3b5d41ffbfc7a4dd2b0632f9ee33f3b93f36
copyleftie <copyleftie@protonmail.com>
2018-02-10 16:59:48 -0500

    tabbed: use base16-woodland; fix build warnings
    I disabled some functionality for when I was still using surf. That's no longer
    relevant and it caused build warnings, so I've uncommented the lines.

--- srcpkgs/tabbed/files/config.h
+++ srcpkgs/tabbed/files/config.h
@@ -2,12 +2,12 @@
 
 /* appearance */
 static const char font[]        = "Inconsolata:size=11";
-static const char* normbgcolor  = "#222222";
-static const char* normfgcolor  = "#bbbbbb";
-static const char* selbgcolor   = "#005577";
-static const char* selfgcolor   = "#eeeeee";
-static const char* urgbgcolor   = "#111111";
-static const char* urgfgcolor   = "#cc0000";
+static const char* normbgcolor  = "#231e18";
+static const char* normfgcolor  = "#9d8b70";
+static const char* selbgcolor   = "#48413a";
+static const char* selfgcolor   = "#cabcb1";
+static const char* urgbgcolor   = "#231e18";
+static const char* urgfgcolor   = "#d35c5c";
 static const char before[]      = "<";
 static const char after[]       = ">";
 static const char titletrim[]   = "...";
@@ -36,14 +36,14 @@ static Bool npisrelative  = True;
 #define MODKEY ControlMask
 static Key keys[] = {
 	/* modifier             key        function     argument */
-	//{ MODKEY|ShiftMask,     XK_Return, focusonce,   { 0 } },
+	{ MODKEY|ShiftMask,     XK_Return, focusonce,   { 0 } },
 	{ MODKEY|ShiftMask,     XK_Return, spawn,       { 0 } },
 
 	{ MODKEY|ShiftMask,     XK_l,      rotate,      { .i = +1 } },
 	{ MODKEY|ShiftMask,     XK_h,      rotate,      { .i = -1 } },
 	{ MODKEY|ShiftMask,     XK_j,      movetab,     { .i = -1 } },
 	{ MODKEY|ShiftMask,     XK_k,      movetab,     { .i = +1 } },
-	//{ MODKEY,               XK_Tab,    rotate,      { .i = 0 } },
+	{ MODKEY,               XK_Tab,    rotate,      { .i = 0 } },
 
 	{ MODKEY,               XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
 	{ MODKEY,               XK_1,      move,        { .i = 0 } },
@@ -59,8 +59,8 @@ static Key keys[] = {
 
 	{ MODKEY,               XK_q,      killclient,  { 0 } },
 
-	//{ MODKEY,               XK_u,      focusurgent, { 0 } },
-	//{ MODKEY|ShiftMask,     XK_u,      toggle,      { .v = (void*) &urgentswitch } },
+	{ MODKEY,               XK_u,      focusurgent, { 0 } },
+	{ MODKEY|ShiftMask,     XK_u,      toggle,      { .v = (void*) &urgentswitch } },
 
-	//{ 0,                    XK_F11,    fullscreen,  { 0 } },
+	{ 0,                    XK_F11,    fullscreen,  { 0 } },
 };
