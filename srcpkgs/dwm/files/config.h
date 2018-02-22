950543233c9e3aa9503d17f9136257e8b80659d1
copyleftie <copyleftie@protonmail.com>
2018-02-21 22:48:23 -0500

    dwm: adjust st -c VISUAL -e vi rules

--- srcpkgs/dwm/files/config.h
+++ srcpkgs/dwm/files/config.h
@@ -34,6 +34,7 @@ static const Rule rules[] = {
 	{ "mpv",         NULL,       NULL,       0,            1,           -1 },
 	{ "Tor Browser", NULL,       NULL,       0,            1,           -1 },
 	{ "VISUAL",      NULL,       NULL,       0,            1,           -1 },
+	{ NULL,          "VISUAL",   NULL,       0,            1,           -1 },
 	{ NULL,          NULL,       "QEMU",     0,            1,           -1 },
 };
 
