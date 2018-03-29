4bd6f35cf8111d97a7e081ece2215e5f27d1c44e
copyleftie <copyleftie@protonmail.com>
2018-03-29 16:41:43 -0400

    dmenu: use acme(1) - like  colorscheme

--- srcpkgs/dmenu/files/config.h
+++ srcpkgs/dmenu/files/config.h
@@ -4,14 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Inconsolata:size=14"
+	"Inconsolata:size=13"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#000000", "#ffffeb" },
+	[SchemeSel] = { "#000000", "#eaed8d" },
+	[SchemeOut] = { "#ffffeb", "#eaed8d" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
